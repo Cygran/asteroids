83fc0b30d74e65805d4e913994f478b2e85f2e88
Cygran <apcanning90@gmail.com>
2025-02-09 00:16:51 +0000

    Working Clock Limiting fps to 60 via .tick. Updaing delta time to dt variable via get_time.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (000, 000, 000)
     while True:
@@ -11,6 +13,8 @@ def main():
                 return
         screen.fill(color)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.get_time() / 1000
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
