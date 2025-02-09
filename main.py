b1a3d9b25c010e0debc4072f10d76e15380beb0b
Cygran <apcanning90@gmail.com>
2025-02-09 11:09:43 +0000

    Defined Classes and Modules to generate player, added drawing of player to game loop.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,11 +8,13 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (000, 000, 000)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color)
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.get_time() / 1000
