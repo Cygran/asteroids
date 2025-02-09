1a4f93d559ee87cbfd411e9e8566bdeabd095dca
Cygran <apcanning90@gmail.com>
2025-02-09 15:34:01 +0000

    Imported Shot class, Created container group, defined container groups for shots. as main increases in size added some comments for my own comfort

--- main.py
+++ main.py
@@ -4,21 +4,33 @@ from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
+    #Set Some initial Variables
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    color = (000, 000, 000)
+    bg_color = (000, 000, 000)
+    
+    #Group Containers
     updatables = pygame.sprite.Group()
     drawables = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    #Set Groups for Object Instances based on Classes
     AsteroidField.containers = (updatables)
     Asteroid.containers = (asteroids, updatables, drawables)
     Player.containers = (updatables, drawables)
+    Shot.containers = (shots, updatables, drawables)
+    
+    #Initialize Asteroid Spawning and Player
     field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    #Game Loop Start
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -28,12 +40,14 @@ def main():
             if asteroid.collision_logic(player):
                 print("Game over!")
                 sys.exit()
-        screen.fill(color)
+        screen.fill(bg_color)
         for drawable in drawables:
             drawable.draw(screen)
         pygame.display.flip()
         clock.tick(60)
         dt = clock.get_time() / 1000
+    #Game Loop End
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
