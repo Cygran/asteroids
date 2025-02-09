294a965c3a128e897687516ea08062d220c37936
Cygran <apcanning90@gmail.com>
2025-02-09 15:21:30 +0000

    Built Shot Class

---
+++ shot.py
@@ -0,0 +1,16 @@
+import pygame
+from circleshape import *
+from constants import SHOT_RADIUS
+
+class Shot(CircleShape):
+    def __init__(self, x, y, velocity):
+        super().__init__(x, y)
+        self.color = (255, 255, 255)
+        self.radius = SHOT_RADIUS
+        self.velocity = velocity
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, self.position, self.radius, width=2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
