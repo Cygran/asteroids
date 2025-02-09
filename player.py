c0db1d75c0aa30e5eac59a11d5164ba9d21bdde4
Cygran <apcanning90@gmail.com>
2025-02-09 15:56:23 +0000

    Created shoot method to "spawn" Shot added handling for "SPACE" input to trigger shoot method

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
-from circleshape import *;
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED;
+from circleshape import *
+from constants import *
+from shot import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -33,7 +34,15 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot_direction = pygame.Vector2(0, 1)
+        shot_direction = shot_direction.rotate(-self.rotation)
+        shot_velocity = shot_direction * PLAYER_SHOOT_SPEED
+        shot = Shot(self.x, self.y, shot_velocity)
